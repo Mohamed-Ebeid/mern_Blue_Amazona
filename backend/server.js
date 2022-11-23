d102a6131052ae053fbdb2956278ae97fab1844d
Umst gh <mohmedalhkim@gmail.com>
2022-11-23 10:34:49 +0200

    vercel file v03 with other things

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,56 @@
-import express from 'express';
-import data from './data.js';
-import path from 'path';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import seedRouter from './Routes/seedRoutes.js';
-import productRouter from './Routes/productRoutes.js';
-import userRouter from './Routes/userRoutes.js';
-import orderRouter from './Routes/orderRoutes.js';
-import uploadRouter from './Routes/uploadRoutes.js';
+import express from "express";
+import data from "./data.js";
+import path from "path";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import seedRouter from "./Routes/seedRoutes.js";
+import productRouter from "./Routes/productRoutes.js";
+import userRouter from "./Routes/userRoutes.js";
+import orderRouter from "./Routes/orderRoutes.js";
+import uploadRouter from "./Routes/uploadRoutes.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODM_URL)
-.then(()=> {console.log('Connected to Mango DB')})
-.catch((e)=> {console.log(e.message)});
+mongoose
+	.connect(process.env.MONGODM_URL)
+	.then(() => {
+		console.log("Connected to Mango DB");
+	})
+	.catch((e) => {
+		console.log(e.message);
+	});
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/keys/paypal', (req, res)=>{
-	res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+app.get("/api/keys/paypal", (req, res) => {
+	res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.use('/api/seed', seedRouter);
+app.use("/api/seed", seedRouter);
 
-app.use('/api/products', productRouter);
+app.use("/api/products", productRouter);
 
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 
-app.use('/api/orders', orderRouter);
+app.use("/api/orders", orderRouter);
 
-app.use('/api/uploads', uploadRouter )
+app.use("/api/uploads", uploadRouter);
 
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res)=>
-  	res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-  	)
- 	
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+	res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
-
-app.use((err, req, res, next)=>{
-	res.status(500).send({message:err.message});
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-	console.log(`Running on http://localhost:${port}`)
-})
-
+app.listen(port, () => {
+	console.log(`Running on http://localhost:${port}`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import {ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter, Route, Routes} from "react-router-dom"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SingleProduct from "./pages/SingleProduct";
-import Container from 'react-bootstrap/Container';
-import NavbarComp from './components/NavbarComp';
+import Container from "react-bootstrap/Container";
+import NavbarComp from "./components/NavbarComp";
 import Cart from "./pages/Cart";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
@@ -21,63 +21,125 @@ import EditUser from "./pages/EditUser";
 import ProductsList from "./pages/ProductsList";
 import EditProduct from "./pages/EditProduct";
 import AllOrders from "./pages/AllOrders";
-import {UserProtectedRoute, AdminRoute} from './components/ProtectedRoute';
-
+import { UserProtectedRoute, AdminRoute } from "./components/ProtectedRoute";
 
 function App() {
-
   return (
-  <BrowserRouter>
-    <div className= "d-flex flex-column site-container">
-      <ToastContainer position="top-right" limit={1}/>
+    <BrowserRouter basename="/">
+      <div className="d-flex flex-column site-container">
+        <ToastContainer position="top-right" limit={1} />
         <header>
-          <NavbarComp/>
+          <NavbarComp />
         </header>
-      <main>
-        <Container className="mt-3">
-          <Routes>
-            <Route path="/product/:slug" element = {<SingleProduct/>}/>
-            <Route path="/shipping" element = {<ShippingAddress/>}/>
-            <Route path="/profile" element = 
-              {<UserProtectedRoute> <Profile/> </UserProtectedRoute>}/>
-            <Route path="/payment" element = 
-              {<UserProtectedRoute><PaymentMethod/></UserProtectedRoute>}/>
-            <Route path="/order/:id" element = 
-              {<UserProtectedRoute><PreviewOrders/></UserProtectedRoute>}/>
-            <Route path="/signin" element = {<SigninPage/>}/>
-            <Route path="/signup" element = {<SignupPage/>}/>
-            <Route path="/placeorder" element = {<PlaceOrder/>}/>
-            <Route path="/search" element = {<SearchPage/>}/>
-            <Route path="/orderhistory" element = 
-              {<UserProtectedRoute> <OrdersHistory/> </UserProtectedRoute>}/>
-            <Route path="/cart" element = {<Cart/>}/>
-            <Route path="/" element = {<Home/>}/>
-            <Route path="/admin/dashboard" element=
-              {<AdminRoute> <Dashboard/> </AdminRoute>} />
-            <Route path="/admin/userlist" element=
-              {<AdminRoute> <AllUsers/> </AdminRoute>} />
-            <Route path="/admin/user/:id/edit" element=
-              {<AdminRoute> <EditUser/> </AdminRoute>} />
-            <Route path="/admin/productlist" element=
-              {<AdminRoute> <ProductsList/> </AdminRoute>} />
-            <Route path="/admin/product/:id/edit" element=
-              {<AdminRoute> <EditProduct/> </AdminRoute>} />
-            <Route path="/admin/orderlist" element=
-              {<AdminRoute> <AllOrders/> </AdminRoute>} />
-          </Routes>
-        </Container>
-      </main>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<SingleProduct />} />
+              <Route path="/shipping" element={<ShippingAddress />} />
+              <Route
+                path="/profile"
+                element={
+                  <UserProtectedRoute>
+                    {" "}
+                    <Profile />{" "}
+                  </UserProtectedRoute>
+                }
+              />
+              <Route
+                path="/payment"
+                element={
+                  <UserProtectedRoute>
+                    <PaymentMethod />
+                  </UserProtectedRoute>
+                }
+              />
+              <Route
+                path="/order/:id"
+                element={
+                  <UserProtectedRoute>
+                    <PreviewOrders />
+                  </UserProtectedRoute>
+                }
+              />
+              <Route path="/signin" element={<SigninPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/placeorder" element={<PlaceOrder />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route
+                path="/orderhistory"
+                element={
+                  <UserProtectedRoute>
+                    {" "}
+                    <OrdersHistory />{" "}
+                  </UserProtectedRoute>
+                }
+              />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/admin/dashboard"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <Dashboard />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/admin/userlist"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <AllUsers />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/admin/user/:id/edit"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <EditUser />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/admin/productlist"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <ProductsList />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/admin/product/:id/edit"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <EditProduct />{" "}
+                  </AdminRoute>
+                }
+              />
+              <Route
+                path="/admin/orderlist"
+                element={
+                  <AdminRoute>
+                    {" "}
+                    <AllOrders />{" "}
+                  </AdminRoute>
+                }
+              />
+            </Routes>
+          </Container>
+        </main>
 
         <footer>
-          <div className="text-center product-details">
-            All rights reserved
-          </div>
-        </footer>  
-
-    </div>
-  </BrowserRouter>
+          <div className="text-center product-details">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
-
 
 export default App;
