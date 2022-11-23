f9e5adc38275a52deca02d0a5ec7065000127755
Umst gh <mohmedalhkim@gmail.com>
2022-11-23 14:04:56 +0200

    first commit

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ import data from "./data.js";
 import path from "path";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
 import seedRouter from "./Routes/seedRoutes.js";
 import productRouter from "./Routes/productRoutes.js";
 import userRouter from "./Routes/userRoutes.js";
 import orderRouter from "./Routes/orderRoutes.js";
 import uploadRouter from "./Routes/uploadRoutes.js";
 
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 dotenv.config();
 
 mongoose
@@ -20,9 +25,6 @@ mongoose
 		console.log(e.message);
 	});
 
-const app = express();
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/keys/paypal", (req, res) => {
@@ -39,11 +41,12 @@ app.use("/api/orders", orderRouter);
 
 app.use("/api/uploads", uploadRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-	res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+// const __dirname = path.resolve();
+// console.log(__dirname);
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+// app.get("*", (req, res) =>
+// 	res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
 
 app.use((err, req, res, next) => {
 	res.status(500).send({ message: err.message });
